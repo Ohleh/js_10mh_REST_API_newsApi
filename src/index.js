feb9d2c60ae550304927efb306cf2475e8a181ea
Ohleh <oleg.kolischuk@gmail.com>
2024-04-10 17:26:56 +0300

    axios cat

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// https://thecatapi.com
+// 'api_key=live_HLM1hsQ0tkLTQgU4vMq98s2XjilsTWNx2KVDtgfwRZOhNbaF9uBwUUnvoBxWLSc5'
+// https://api.thecatapi.com/v1/images/search
+
+// import { numb, numr } from './js/cat-api';
+// console.log(numb);
+// console.log(numr);
+
+const nuumb = 'hi-hi';
+console.log(nuumb);
+
+// const KEY =
+//   'live_HLM1hsQ0tkLTQgU4vMq98s2XjilsTWNx2KVDtgfwRZOhNbaF9uBwUUnvoBxWLSc5';
+// const BASE_URL = 'https://api.thecatapi.com/v1/images';
+// console.log('hi');
+
+// fetch(`${BASE_URL}/search?breed_ids=mcoo&api_key=${KEY}`)
+//   .then(res => {
+//     if (!res.ok) {
+//       throw new Error(res.status);
+//     }
+//     return res.json();
+//   })
+//   .then(data => console.log(data))
+//   .catch(e => console.log('erroe', e));
+
+// import axios from 'axios';
+
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_HLM1hsQ0tkLTQgU4vMq98s2XjilsTWNx2KVDtgfwRZOhNbaF9uBwUUnvoBxWLSc5';
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+
+// axios
+//     .get(`${BASE_URL}/breeds`)
+//   .then(res => {
+//     console.log(res);
+//     breedsList = res;
+//     console.log(breedsList.data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
---
+++ src/js/cat-api.js
@@ -0,0 +1,29 @@
+// import axios from 'axios';
+
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_HLM1hsQ0tkLTQgU4vMq98s2XjilsTWNx2KVDtgfwRZOhNbaF9uBwUUnvoBxWLSc5';
+// const BASE_URL = 'https://api.thecatapi.com/v1';
+
+// let breedsList;
+
+// const breedsGet = data => {
+//   breedsList = data.map(breed => ({ id: breed.id, name: breed.name }));
+// };
+
+// axios
+//   .get(`${BASE_URL}/breeds`)
+//   .then(({ data }) => {
+//     breedsGet(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// export default function myFunc() {
+//   console.log('hi');
+//   // ...
+// }
+
+const numb = 'hi-hi';
+const numr = 5;
+// export { numb, numr };
